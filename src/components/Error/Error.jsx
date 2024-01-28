e052af7c9463d9937095963301be60f4a2ef4ccf
parthasarathi019 <parthasarathaie@gmail.com>
2024-01-28 13:59:59 +0600

    kjgiuv

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Error = () => {
   return (
     <div>
-      <img src="https://i.ibb.co/2qVXs9Y/image-2.png" alt="" />
+      {/* HOST ERROR IMG */}
+      {/* <img src="https://i.ibb.co/2qVXs9Y/image-2.png" alt="" /> */}
+
+       {/* CODING ERROR IMG */}
+      <img src="https://i.ibb.co/bPb5sNq/image-4.png" alt="" />
     </div>
   )
 }
